b83483d8f5d37cc9a54e470ec5292a966e33a029
Ebenezer Scrooge <1641367382@qq.com>
2018-12-18 11:40:23 +0800

    Revert "Update lcd.c"
    This reverts commit cb192685ee4a84b3be3950de5ee153be07c2562d.

--- USER/lcd.c
+++ USER/lcd.c
@@ -493,7 +493,7 @@ GPIO_SetBits(TCH_SPI_CSN3_3_GPIO_PORT, TCH_SPI_CSN3_3_PIN);
 * Return: FW upgrate result
 * Call: external
 */
-__weak ErrorStatus RA_Program_FW(void)
+ErrorStatus RA_Program_FW(void)
 {
 uint8_t i=0;
 uint8_t k=0;
