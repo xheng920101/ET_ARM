b822870398b4c81c4851bc819dc2be6843e09a2e
Ebenezer Scrooge <1641367382@qq.com>
2019-01-23 09:06:11 +0800

    clean branch to restart from V2P5

--- USER/test_od.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TEST_OD_H
-#define __TEST_OD_H
-
-/*********************************************************************************
- * Function statement for external call
- */
-void Test_OD(void);
-
-#endif /* __TEST_OD_H */
--- USER/test_oqc1.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TEST_OQC1_H
-#define __TEST_OQC1_H
-
-/*********************************************************************************
- * Function statement for external call
- */
-void Test_OQC1(void);
-
-#endif /* __TEST_OQC1_H */
